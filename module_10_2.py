64bcdfb2bfe04df4a3d31afe93ad53faa0c3e70b
SafonovValera <o33o.post@gmail.com>
2025-01-02 11:19:54 +0300

    module_10_2.py

--- module_10_2.py
+++ module_10_2.py
@@ -43,7 +43,7 @@ class Knight(Thread):
 # Создание класса
 
 first_knight = Knight('Sir Lancelot', 10)
-second_knight = Knight("Sir Galahad", 20)
+second_knight = Knight("Sir Galahad", 20, 200)
 
 first_knight.start()
 second_knight.start()
